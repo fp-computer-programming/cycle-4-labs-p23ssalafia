7707dbbf9d564215242f53c21a0e06fb8a1aa078
SeanSalafia <p23ssalafia@fairfieldprep.org>
2022-10-11 10:34:21 -0400

    Submission for Lab 4-1

---
+++ Lab_4-1.py
@@ -0,0 +1,11 @@
+#Author: Sean Salafia 10/11/22
+
+#User inputs their First and Last Name:
+First_Name = input ('Please write your first name')
+Last_Name = input ('Please write you last name')
+
+# Recieved user input and prints the full name with an added space.
+Full_Name = First_Name + ' ' + Last_Name
+print(Full_Name)
+
+# Does not seem to be any issue with the output when ' ' is put in.
